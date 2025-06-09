dfec8be7514971ee06a18f9347290bdbe84e012b
Hamza-Noah <hamza.nouh97@gmail.com>
2025-06-09 10:15:26 +0400

    Refactor: Removed Dark Mode button from Form to maintain focus on form functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,41 @@
-import Form from './Form'
-import Sidebar from './Sidebar'
-import { IconSun, IconMoon } from '@tabler/icons-react';
-import { useMantineColorScheme, ActionIcon, useComputedColorScheme  } from '@mantine/core';
-
+import Form from "./Form";
+import Sidebar from "./Sidebar";
+import { IconSun, IconMoon } from "@tabler/icons-react";
+import {
+  useMantineColorScheme,
+  ActionIcon,
+  useComputedColorScheme,
+} from "@mantine/core";
 
 export default function App() {
   const computedColorScheme = useComputedColorScheme();
   const { setColorScheme } = useMantineColorScheme();
 
-
   return (
     <>
-    <div className="d-flex">
+      <div className="d-flex">
+        <div>
+          <Sidebar />
+        </div>
+        <div style={{ flex: 3 }}>
+          <ActionIcon
+            variant="outline"
+            color={computedColorScheme === "dark" ? "yellow" : "blue"}
+            onClick={() =>
+              setColorScheme(computedColorScheme === "dark" ? "light" : "dark")
+            }
+            title="Toggle color scheme"
+          >
+            {computedColorScheme === "dark" ? (
+              <IconSun size={18} />
+            ) : (
+              <IconMoon size={18} />
+            )}
+          </ActionIcon>
 
-    <div>
-      <Sidebar/>
-    </div>
-    <div style={{flex: 3}}>
-<ActionIcon
-      variant="outline"
-      color={computedColorScheme === 'dark' ? 'yellow' : 'blue'}
-      onClick={() =>
-        setColorScheme(computedColorScheme === 'dark' ? 'light' : 'dark')
-      }
-      title="Toggle color scheme"
-    >
-      {computedColorScheme === 'dark' ? <IconSun size={18} /> : <IconMoon size={18} />}
-    </ActionIcon>
-
-      <Form/>
-    </div>
-    </div>
+          <Form />
+        </div>
+      </div>
     </>
-  )
+  );
 }
